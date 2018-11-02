0b4bf68c22d381d735bd1da5992396dff92b0cad
wqzwh <wangziqihappy@163.com>
2018-11-02 16:15:04 +0800

    feat：头像组件添加

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0,
     'eol-last': 0,
-    'semi': 0
+    'semi': 0,
+    'no-trailing-spaces': 0
   }
 }
